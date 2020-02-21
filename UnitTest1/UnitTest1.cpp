892de4cf05865e84b5233eaf9de5d49c46f330d9
Lane, Derek <Derek.Lane@elekta.com>
2020-02-20 19:42:55 -0600

    fixing build of unit test

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "WarpTpsLib.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,5 +13,63 @@ namespace UnitTest1
 		TEST_METHOD(TestMethod1)
 		{
 		}
+
+		TEST_METHOD(TestAddLandmark)
+		{
+			Logger::WriteMessage("TestAddLandmark");
+
+			auto tpsTransform = new CTPSTransform();			
+			tpsTransform->AddLandmark(CVectorD<3>(0.0, 0.0, 0.0));
+			tpsTransform->AddLandmark(CVectorD<3>(1.0, 0.0, 0.0));
+			tpsTransform->AddLandmark(CVectorD<3>(0.0, 1.0, 0.0));
+			tpsTransform->AddLandmark(CVectorD<3>(0.0, 0.0, 1.0));
+
+			Assert::IsTrue(tpsTransform->GetLandmark(0, 0).IsApproxEqual(CVectorD<3>(0.0, 0.0, 0.0)));
+			Assert::IsTrue(tpsTransform->GetLandmark(0, 1).IsApproxEqual(CVectorD<3>(1.0, 0.0, 0.0)));
+			Assert::IsTrue(tpsTransform->GetLandmark(0, 2).IsApproxEqual(CVectorD<3>(0.0, 1.0, 0.0)));
+			Assert::IsTrue(tpsTransform->GetLandmark(0, 3).IsApproxEqual(CVectorD<3>(0.0, 0.0, 1.0)));
+
+			Assert::IsTrue(tpsTransform->GetLandmark(1, 0).IsApproxEqual(CVectorD<3>(0.0, 0.0, 0.0)));
+			Assert::IsTrue(tpsTransform->GetLandmark(1, 1).IsApproxEqual(CVectorD<3>(1.0, 0.0, 0.0)));
+			Assert::IsTrue(tpsTransform->GetLandmark(1, 2).IsApproxEqual(CVectorD<3>(0.0, 1.0, 0.0)));
+			Assert::IsTrue(tpsTransform->GetLandmark(1, 3).IsApproxEqual(CVectorD<3>(0.0, 0.0, 1.0)));
+
+			Logger::WriteMessage("Done TestAddLandmark");
+		}
+
+		TEST_METHOD(TestCalculateWarp)
+		{
+			Logger::WriteMessage("TestAddLandmark");
+
+			auto tpsTransform = new CTPSTransform();
+			tpsTransform->AddLandmark(CVectorD<3>(0.0, 0.0, 0.0));
+			tpsTransform->AddLandmark(CVectorD<3>(1.0, 0.0, 0.0));
+			tpsTransform->AddLandmark(CVectorD<3>(0.0, 1.0, 0.0));
+			tpsTransform->AddLandmark(CVectorD<3>(0.0, 0.0, 1.0));
+
+			CVectorD<3> vOriginal(0.0, 0.0, 0.0);
+			CVectorD<3> vTransformed;
+			tpsTransform->Eval(vOriginal, vTransformed, 0.0);
+			Assert::IsTrue(vOriginal.IsApproxEqual(vTransformed));
+
+			Logger::WriteMessage("Done TestAddLandmark");
+		}
+
+		TEST_METHOD(TestApplyWarpAtLandmark)
+		{
+		}
+
+		TEST_METHOD(TestApplyInverseWarpAtLandmark)
+		{
+		}
+
+		TEST_METHOD(TestResampleUniformImage)
+		{
+		}
+
+		TEST_METHOD(TestResampleImageHistogram)
+		{
+		}
+
 	};
 }
--- WarpTpsLib/WarpTpsLib.h
+++ WarpTpsLib/WarpTpsLib.h
@@ -3,10 +3,6 @@
 
 #pragma once
 
-#ifndef __AFXWIN_H__
-	#error "include 'pch.h' before including this file for PCH"
-#endif
-
 #include "resource.h"		// main symbols
 
 #ifdef WARPTPSLIB_EXPORTS
