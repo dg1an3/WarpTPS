0139ba07e82f9c1705bff8fc1b3db6265d342d21
Lane, Derek <Derek.Lane@elekta.com>
2020-02-24 21:41:06 -0600

    formatting c++ code

--- BoostConsoleApplication1/connection_manager.cpp
+++ BoostConsoleApplication1/connection_manager.cpp
@@ -15,26 +15,26 @@
 #include <boost/bind.hpp>
 
 namespace http {
-namespace server {
+	namespace server {
 
-void connection_manager::start(connection_ptr c)
-{
-  connections_.insert(c);
-  c->start();
-}
+		void connection_manager::start(connection_ptr c)
+		{
+			connections_.insert(c);
+			c->start();
+		}
 
-void connection_manager::stop(connection_ptr c)
-{
-  connections_.erase(c);
-  c->stop();
-}
+		void connection_manager::stop(connection_ptr c)
+		{
+			connections_.erase(c);
+			c->stop();
+		}
 
-void connection_manager::stop_all()
-{
-  std::for_each(connections_.begin(), connections_.end(),
-      boost::bind(&connection::stop, _1));
-  connections_.clear();
-}
+		void connection_manager::stop_all()
+		{
+			std::for_each(connections_.begin(), connections_.end(),
+				boost::bind(&connection::stop, _1));
+			connections_.clear();
+		}
 
-} // namespace server
+	} // namespace server
 } // namespace http
